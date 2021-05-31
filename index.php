b46b9ccc46703356a6dab0a6aabc13fa2ddc3c31
SultanRaffy <sraffy15@gmail.com>
2021-05-31 16:57:56 +0700

    Praktikum Minggu 4

---
+++ index.php
@@ -0,0 +1,103 @@
+<?php
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Trails of Cold Steel IV</title>
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "SoftwareApplication",
+      "name": "Trails of Cold Steel IV",
+      "operatingSystem": "Windows",
+      "applicationCategory": "GameApplication",
+      "fileSize":"25gb",
+      "aggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": "4.7",
+        "ratingCount": "725"
+      },
+      "offers": {
+        "@type": "Offer",
+        "price": "389.999",
+        "priceCurrency": "IDR"
+      },
+      "GamePlayMode":{
+      	"@type": "playMode",
+        "playMode": "Single Player"
+      }
+    }
+    </script>
+</head>
+<body>
+    <header id="home">
+        <nav>
+            <ul class="navhead">
+                <li><a href="#Introduction">Introduction</a></li>
+                <li><a href="#Characters">Characters</a></li>
+                <li><a href="#Media">Media</a></li>
+                <li><a href="#Product" class="buy">Purchase Now</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+      <div class="container">
+        <div class="card">
+          <h1>Trails of Cold Steel IV</h1>
+          <img class="img" src="coldsteel4.jpg" alt=""> 
+          <p>The long awaited finale to the epic engulfing a continent comes to a head in the final chapter of the Trails of Cold Steel saga!</p>
+          <br>
+          <div class="about">
+            <div class="about-content">
+              <h2>About This Game</h2>
+              <p><b>The Ultimate Class Reunion</b> – Boasting the largest roster in series history, heroes from all over seek to join the cause! The heroes of Class VII unite with Crossbell's Special Support Section and even the heroes of Liberl.</p>
+              <p><b>Battle Most Refined</b> – New and returning systems join the already polished combat of The Legend of Heroes series, including the ability to summon giant mechs to the field for devastating attacks, use Auto Battle for more expedient combat, and utilize Lost Arts, the most powerful Orbal magic capable of turning the tide of battle.</p>
+              <br>
+            </div>
+          </div>
+          <h2>System Requirement</h2>
+          <table>
+            <tr>
+              <th></th>
+              <th>Minimum</th>
+              <th>Maximum</th>
+            </tr>
+            <tr>
+              <td>OS</td>
+              <td>Windows 10/8.1/7 64-bit</td>
+              <td>Windows 10/8.1/7 64-bit</td>
+            </tr>
+            <tr>
+              <td>Processor</td>
+              <td>Core i3-2100 3.10 GHz</td>
+              <td>AMD FX-8320 8-Core</td>
+            </tr>
+            <tr>
+              <td>Memory</td>
+              <td>8 GB RAM</td>
+              <td>16 GB RAM</td>
+            </tr>
+            <tr>
+              <td>Graphics</td>
+              <td>Geforce GTX 650 Ti</td>
+              <td>Radeon R7 370</td>
+            </tr>
+            <tr>
+              <td>Storage</td>
+              <td>25 GB available space</td>
+              <td>25 GB available space</td>
+            </tr>
+        </table>
+          <hr class="solid">
+        </div>
+      </div>
+    </main>
+</body>
+
+</html>
